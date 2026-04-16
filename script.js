07e4dccb670508bd090a791dff6649f6c5a61504
hackpkziin-sketch <hackpkziin@gmail.com>
2026-04-16 17:06:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+let menu=document.getElementById("menu");
+
+menu.style.right =
+(menu.style.right==="0px") ? "-260px" : "0px";
+}
+
+function enviar(){
+alert("Denúncia enviada!");
+}
